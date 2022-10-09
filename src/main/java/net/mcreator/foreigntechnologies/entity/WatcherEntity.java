9fc4c50414477d98b8ad8888f578d6d6e4682a4c
SomeFurryCass <Backup@DESKTOP-N3VDCU0.fios-router.home>
2022-10-09 02:29:56 -0400

    watchernerf >~< (god damm these things are horrifying)

--- src/main/java/net/mcreator/foreigntechnologies/entity/WatcherEntity.java
+++ src/main/java/net/mcreator/foreigntechnologies/entity/WatcherEntity.java
@@ -8,19 +8,22 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 
+import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.navigation.FlyingPathNavigation;
+import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
 import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.ai.control.FlyingMoveControl;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -44,6 +47,8 @@ import net.mcreator.foreigntechnologies.init.ForeignTechnologiesModItems;
 import net.mcreator.foreigntechnologies.init.ForeignTechnologiesModEntities;
 
 import java.util.Set;
+import java.util.Random;
+import java.util.EnumSet;
 
 @Mod.EventBusSubscriber
 public class WatcherEntity extends Monster {
@@ -81,16 +86,65 @@ public class WatcherEntity extends Monster {
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
+		this.goalSelector.addGoal(1, new Goal() {
+			{
+				this.setFlags(EnumSet.of(Goal.Flag.MOVE));
+			}
+
+			public boolean canUse() {
+				if (WatcherEntity.this.getTarget() != null && !WatcherEntity.this.getMoveControl().hasWanted()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+
+			@Override
+			public boolean canContinueToUse() {
+				return WatcherEntity.this.getMoveControl().hasWanted() && WatcherEntity.this.getTarget() != null
+						&& WatcherEntity.this.getTarget().isAlive();
+			}
+
+			@Override
+			public void start() {
+				LivingEntity livingentity = WatcherEntity.this.getTarget();
+				Vec3 vec3d = livingentity.getEyePosition(1);
+				WatcherEntity.this.moveControl.setWantedPosition(vec3d.x, vec3d.y, vec3d.z, 1);
+			}
+
+			@Override
+			public void tick() {
+				LivingEntity livingentity = WatcherEntity.this.getTarget();
+				if (WatcherEntity.this.getBoundingBox().intersects(livingentity.getBoundingBox())) {
+					WatcherEntity.this.doHurtTarget(livingentity);
+				} else {
+					double d0 = WatcherEntity.this.distanceToSqr(livingentity);
+					if (d0 < 16) {
+						Vec3 vec3d = livingentity.getEyePosition(1);
+						WatcherEntity.this.moveControl.setWantedPosition(vec3d.x, vec3d.y, vec3d.z, 1);
+					}
+				}
+			}
+		});
+		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 0.8, 20) {
+			@Override
+			protected Vec3 getPosition() {
+				Random random = WatcherEntity.this.getRandom();
+				double dir_x = WatcherEntity.this.getX() + ((random.nextFloat() * 2 - 1) * 16);
+				double dir_y = WatcherEntity.this.getY() + ((random.nextFloat() * 2 - 1) * 16);
+				double dir_z = WatcherEntity.this.getZ() + ((random.nextFloat() * 2 - 1) * 16);
+				return new Vec3(dir_x, dir_y, dir_z);
+			}
+		});
+		this.goalSelector.addGoal(3, new MeleeAttackGoal(this, 1.2, false) {
 			@Override
 			protected double getAttackReachSqr(LivingEntity entity) {
 				return (double) (4.0 + entity.getBbWidth() * entity.getBbWidth());
 			}
 		});
-		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
-		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
 		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
-		this.goalSelector.addGoal(5, new FloatGoal(this));
+		this.targetSelector.addGoal(5, new NearestAttackableTargetGoal(this, Player.class, false, false));
+		this.targetSelector.addGoal(6, new HurtByTargetGoal(this));
 	}
 
 	@Override
@@ -192,14 +246,14 @@ public class WatcherEntity extends Monster {
 
 	public static AttributeSupplier.Builder createAttributes() {
 		AttributeSupplier.Builder builder = Mob.createMobAttributes();
-		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.7);
+		builder = builder.add(Attributes.MOVEMENT_SPEED, 0.4);
 		builder = builder.add(Attributes.MAX_HEALTH, 100);
 		builder = builder.add(Attributes.ARMOR, 10);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 25);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 100);
 		builder = builder.add(Attributes.KNOCKBACK_RESISTANCE, 0.6);
 		builder = builder.add(Attributes.ATTACK_KNOCKBACK, 0.2);
-		builder = builder.add(Attributes.FLYING_SPEED, 0.7);
+		builder = builder.add(Attributes.FLYING_SPEED, 0.4);
 		return builder;
 	}
 }
