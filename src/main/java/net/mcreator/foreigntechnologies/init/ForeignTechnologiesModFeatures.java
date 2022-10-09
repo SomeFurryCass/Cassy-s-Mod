8adf30d6961f144383fad1a6ce71fc6aad180bb5
SomeFurryCass <Backup@DESKTOP-N3VDCU0.fios-router.home>
2022-10-08 22:07:47 -0400

    stalkfix no really I mean it this time

--- src/main/java/net/mcreator/foreigntechnologies/init/ForeignTechnologiesModFeatures.java
+++ src/main/java/net/mcreator/foreigntechnologies/init/ForeignTechnologiesModFeatures.java
@@ -17,6 +17,9 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.Holder;
 
+import net.mcreator.foreigntechnologies.world.features.SmallstalkFeature;
+import net.mcreator.foreigntechnologies.world.features.LargestalkFeature;
+import net.mcreator.foreigntechnologies.world.features.HugestalkFeature;
 import net.mcreator.foreigntechnologies.world.features.EtherstalktreenormalFeature;
 import net.mcreator.foreigntechnologies.ForeignTechnologiesMod;
 
@@ -32,6 +35,12 @@ public class ForeignTechnologiesModFeatures {
 	public static final RegistryObject<Feature<?>> ETHERSTALKTREENORMAL = register("etherstalktreenormal", EtherstalktreenormalFeature::feature,
 			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, EtherstalktreenormalFeature.GENERATE_BIOMES,
 					EtherstalktreenormalFeature::placedFeature));
+	public static final RegistryObject<Feature<?>> SMALLSTALK = register("smallstalk", SmallstalkFeature::feature, new FeatureRegistration(
+			GenerationStep.Decoration.SURFACE_STRUCTURES, SmallstalkFeature.GENERATE_BIOMES, SmallstalkFeature::placedFeature));
+	public static final RegistryObject<Feature<?>> LARGESTALK = register("largestalk", LargestalkFeature::feature, new FeatureRegistration(
+			GenerationStep.Decoration.SURFACE_STRUCTURES, LargestalkFeature.GENERATE_BIOMES, LargestalkFeature::placedFeature));
+	public static final RegistryObject<Feature<?>> HUGESTALK = register("hugestalk", HugestalkFeature::feature,
+			new FeatureRegistration(GenerationStep.Decoration.SURFACE_STRUCTURES, HugestalkFeature.GENERATE_BIOMES, HugestalkFeature::placedFeature));
 
 	private static RegistryObject<Feature<?>> register(String registryname, Supplier<Feature<?>> feature, FeatureRegistration featureRegistration) {
 		FEATURE_REGISTRATIONS.add(featureRegistration);
